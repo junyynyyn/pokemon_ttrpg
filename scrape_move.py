8ffb12dd4079165e00d0ba655d31082fcf678786
Jun <junyynyyn@users.noreply.github.com>
2024-07-24 22:39:38 +1000

    Added scraping function to scrape moves and convert to ttrpg form

--- scrape_move.py
+++ scrape_move.py
@@ -1,19 +1,82 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import re
+
+base_url = "https://serebii.net/attackdex-sv/"
+url_end = ".shtml"
 
 def scrape_move(move_name: str):
-    # Scrape move from serebii database
-    # Get Attack Name
-    # Get type
-    # Get category (physical, special, status)
-    # Get PP and convert to PP consumption
-    # Get move power, convert to dice
-    # 10-25 1d4
-    # 30-45 2d4
-    # 50-65 3d6
-    # 70-85 4d6
-    # 90-105 5d8
-    # 110-125 5d10
-    # 130-145 5d12
-    # 150 6d12
+    # Remove spaces in move name
+    move_name = re.sub(" ", "", move_name)
+    move_name = move_name.lower()
+    # # Scrape move from serebii database
+    move = {}
+    try:
+        with urllib.request.urlopen(base_url + move_name + url_end) as response:
+            html = response.read()
+            soup = BeautifulSoup(html, 'html.parser')
+            dextables = soup.find_all('table')
+
+            attack_info = dextables[3].find_all('tr')
+            # tr listing:
+            # tr 1 contains attack name, type, category
+            # tr 3 contains Max PP, Base Power, Accuracy
+            # tr 5 contains battle description
+            # tr 7 contains any secondary effects and their effect rate
+
+            # Getting Attack Name
+            attack_name = attack_info[1].find_all('td')[0].text
+            # Using regex to remove non alphanumeric characters
+            attack_name = re.sub('[^A-z1-9\-, ]', "", attack_name)
+            move["attack_name"] = attack_name
+
+            # Getting attack type
+            attack_type = attack_info[1].find_all('td')[1].find('a')['href']
+            # Using regex to remove start and end of typing
+            attack_type = re.search('^/attackdex-sv\/(.*)\.shtml', attack_type).group(1).capitalize()
+            move["type"] = attack_type
+
+            # Getting attack category
+            attack_category = attack_info[1].find_all('td')[2].find('a')['href']
+            attack_category = re.search('^/attackdex-sv\/(.*)\.shtml', attack_category).group(1).capitalize()
+            move["category"] = attack_category
+
+            # Getting PP Max and converting to PP Usage
+            pp_max = int(attack_info[3].find_all('td')[0].text.strip())
+            pp_map = {40: 1, 35: 2, 30: 3, 25: 4, 20: 5, 15: 6, 10: 7, 5: 8}
+            move["pp_usage"] = pp_map[pp_max]
+
+            # Getting Move Power and converting to dice
+            attack_power = attack_info[3].find_all('td')[1].text.strip()
+            print(attack_power)
+            # 10-25 1d4 avg 2
+            # 30-45 2d4 avg 4
+            # 50-65 3d6 avg 9
+            # 70-85 4d6 avg 12
+            # 90-105 5d8 avg 20
+            # 110-125 5d10 avg 25
+            # 130-145 5d12 avg 30
+            # 150-160 6d12 avg 36
+            # 180 8d12 avg avg 48
+            # 200 9d12 avg 54
+            # 250 10d12 avg 60
+            power_thresholds = [25, 45, 65, 85, 105, 125, 145, 160, 180, 200, 250]
+            power_map = ["1d4", "2d4", "3d6", "4d6", "5d8", "5d10", "5d12", "6d12", "8d12", "10d12", "12d12"]
+            for i in range(len(power_thresholds)):
+                if (int(attack_power) < power_thresholds[i]):
+                    move["damage"] = power_map[i]
+                    break
+
+            # Getting move accuracy and converting to d20 roll
+            attack_accuracy = attack_info[3].find_all('td')[2].text.strip()
+            move["accuracy"] = int(int(attack_accuracy) / 5)
+
+            print(move)
+            
+    except:
+        print("Error while scraping page")
     # Get accuracy, convert to accuracy modifier
+    return
+
+if __name__ == "__main__":
+    scrape_move("Fire Blast");
